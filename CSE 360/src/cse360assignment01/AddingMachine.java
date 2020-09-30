f4119ad6d167d5edf9310ee05c82fb184466cc69
YasserDbeis <ydbeis@asu.edu>
2020-09-29 20:34:36 -0700

    Completed the functions as well as added test cases.

--- CSE 360/src/cse360assignment01/AddingMachine.java
+++ CSE 360/src/cse360assignment01/AddingMachine.java
@@ -10,48 +10,74 @@ package cse360assignment01;
  */
 public class AddingMachine {
 	private int total;
+	private String receipt;
 	
 	/**
-	 * Adding Machine has a default constructor setting total instance variable to 0
+	 * Adding Machine has a default constructor setting total instance variable to 0 and receipt to "0"
 	 */
 	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
+		total = 0; 
+		receipt = "0";
 	}
   
 	/**
-	 * getTotal is a method that returns the total instance variable (currently set to 0 in this code)
-	 * @return
+	 * getTotal is a method that returns the total instance variable 
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
   
 	/**
 	 * add is a void method that add's a parameterized input to the total instance variable
-	 * @param value
 	 */
 	public void add (int value) {
-  
+		total += value;
+		receipt += " + " + value;
 	}
 
 	/**
-	 * substract is a void method that subtracts a parameterized input from the total instance variable
+	 * subtract is a void method that subtracts a parameterized input from the total instance variable
 	 */
 	public void subtract (int value) {
-  
+		total -= value;
+		receipt += " - " + value;
 	}
 
 	/**
 	 * toString returns the total in string format
 	 */
 	public String toString () {
-		return "";
+		return receipt;
 	}
 
 	/**
 	 * clear clears the value of total
 	 */
 	public void clear() {
-  
+		total = 0;
+		receipt = "0";
+	}
+}
+
+/**\
+ * TestAddingMachine is a test class with a main method to test the AddingMachine class
+ */
+
+class TestAddingMachine {
+	public static void main(String[] args) {
+		AddingMachine myCalculator = new AddingMachine();
+		myCalculator.add(4);
+		myCalculator.subtract(2);
+		myCalculator.add(5);
+		
+		System.out.println(myCalculator.toString());
+		System.out.println(myCalculator.getTotal());
+		myCalculator.clear();
+		System.out.println(myCalculator.toString());
+		System.out.println(myCalculator.getTotal());
+		myCalculator.subtract(2);
+		myCalculator.add(5);
+		System.out.println(myCalculator.toString());
+		System.out.println(myCalculator.getTotal());
 	}
 }
